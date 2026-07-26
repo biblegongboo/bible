34bce351d06c31002f581cf75ca011012fe1a823
biblegongboo <biblegongboo@gmail.com>
2026-07-25 19:46:00 -0700

    Add files via upload

---
+++ graphic-router.js
@@ -0,0 +1,4 @@
+// Bible Quiz does not use the optional Super Graphic Engine yet.
+export function isSuperGraphicPayload() { return false; }
+export async function preloadSuperGraphicEngine() { return false; }
+export function renderSuperGraphicPayload() { return ''; }
