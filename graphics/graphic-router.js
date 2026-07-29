05e1ca33078475afa8a1d67504fbc3b0f343ea85
biblegongboo <biblegongboo@gmail.com>
2026-07-28 18:43:13 -0700

    Delete graphics directory

--- graphics/graphic-router.js
+++
@@ -1,4 +0,0 @@
-// Bible Quiz does not use the optional Super Graphic Engine yet.
-export function isSuperGraphicPayload() { return false; }
-export async function preloadSuperGraphicEngine() { return false; }
-export function renderSuperGraphicPayload() { return ''; }
